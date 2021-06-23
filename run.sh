64cfb79a76501c85bb23a48427ffd03efc154286
Alexandre LLemes <alexandre.llemes@gmail.com>
2021-06-23 10:00:09 -0300

    #3 - Correções no script de instalação.

--- run.sh
+++ run.sh
@@ -15,19 +15,19 @@ sudo docker-compose up -d --build
 #sudo docker-compose exec php-apache apt -y upgrade
 
 echo 'Desabilita o site default'
-sudo docker-compose exec php-apache sh -c "a2dissite 000-default"
+#sudo docker exec -it php-apache sh -c "a2dissite 000-default"
 
 #echo 'Habilita o site de desenvolvimento'
-sudo docker-compose exec php-apache sh -c "a2ensite meusite.local"
+sudo docker exec -it php-apache sh -c "a2ensite meusite.local"
 
 echo 'Reinicia o apache2'
-sudo docker-compose exec php-apache sh -c "service apache2 reload"
+sudo docker exec -it php-apache sh -c "service apache2 reload"
 
 #echo 'Reinicia o memcached'
 #sudo docker-compose exec memcached service memcached restart
 
 echo 'Modulos habilitados...'
-sudo docker-compose exec php-apache php -m | grep -E 'memcache|oci8|uploadprogress|mcrypt'
+sudo docker exec -it php-apache php -m | grep -E 'memcache|oci8|uploadprogress|mcrypt'
 
 #echo 'Acesso a maquina virtual'
 #sudo docker-compose exec php-apache bash
